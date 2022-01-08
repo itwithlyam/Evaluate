47f4a792859c27bbf4f90644d11013fe05128189
lyam <lyam.mosnier@itwithlyam.co.uk>
2022-01-08 18:40:25 +0000

    Add Shunting Yard Algorithm to utilities

--- util.js
+++ util.js
@@ -36,10 +36,45 @@ class Fault {
 	}
 }
 
+let Yard = (infix) => {
+	let ops = {'+': 1, '-': 1, '*': 2, '/': 2};
+	let peek = (a) => a[a.length - 1];
+	let stack = [];
+  
+	return infix
+	  .split('')
+	  .reduce((output, token) => {
+		if (parseFloat(token)) {
+		  output.push(token);
+		}
+  
+		if (token in ops) {
+		  while (peek(stack) in ops && ops[token] <= ops[peek(stack)])
+			output.push(stack.pop());
+		  stack.push(token);
+		}
+  
+		if (token == '(') {
+		  stack.push(token);
+		}
+  
+		if (token == ')') {
+		  while (peek(stack) != '(')
+			output.push(stack.pop());
+		  stack.pop();
+		}
+  
+		return output;
+	  }, [])
+	  .concat(stack.reverse())
+	  .join(' ');
+  };
+  
 module.exports = {
 	LexicalError,
 	CompilationError,
 	RuntimeError,
 	Fault,
+	Yard,
 	run
 }
