02279884792c7ed30775f233e91ca3a76246999a
lyam <lyam.mosnier@itwithlyam.co.uk>
2022-01-08 21:16:21 +0000

    Bring tests up to date

--- test/math.js
+++ test/math.js
@@ -4,24 +4,27 @@ var expect = require('chai').expect;
 
 describe('Maths', function() {
 
-  context('general equation', function() {
+  context('simple equations', function() {
     it('should return 2', function() {
       expect(start("1 + 1")).to.equal(2)
     })
 		it('should return 1', function() {
-			expect(start("1 * 1")).to.equal(1)
+			expect(start("{1 * 1}")).to.equal(1)
 		})
 		it("should return 2", function() {
-			expect(start("10 / 5")).to.equal(2)
+			expect(start("{10 / 5}")).to.equal(2)
 		})
 		it("should return 4", function() {
-			expect(start("10 - 6")).to.equal(4)
+			expect(start("{10 - 6}")).to.equal(4)
 		})
-		it("should still execute", function() {
-			expect(start("1+               1")).to.equal(2)
-		})
+  })
+  context('complex calculations', function() {
+	  it("should return 44", function() {
+		  expect(start("{11 * (12 - 1)}")).to.equal(44)
+	  })
+	  it("should return 1", function() {
+		  expect(start("{0.5 * 2}")).to.equal(1)
+	  })
   })
   
 })
-
-
