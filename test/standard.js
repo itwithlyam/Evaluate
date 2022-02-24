1ce3835546b95c73018eddd9c9b96bc3b4a6948d
itwithlyam <lyam.mosnier@itwithlyam.co.uk>
2022-02-24 20:55:36 +0000

    Add strings

---
+++ test/standard.js
@@ -0,0 +1,10 @@
+import {run as start} from '../src/util.js'
+import {expect} from 'chai'
+
+describe('Functions', function() {
+	context('standard mathematical functions', function() {
+		it('simplify', function() {
+			start(`simplify("12+12")`)
+		})
+	})
+})
