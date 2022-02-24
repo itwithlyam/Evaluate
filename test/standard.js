4521d91eed028f0f10205750395eea56d39f3e19
itwithlyam <lyam.mosnier@itwithlyam.co.uk>
2022-02-24 22:04:18 +0000

    Add output tests

--- test/standard.js
+++ test/standard.js
@@ -4,7 +4,10 @@ import {expect} from 'chai'
 describe('Functions', function() {
 	context('standard mathematical functions', function() {
 		it('simplify', function() {
-			start(`simplify("12x*17x")`)
+			expect(start(`simplify("12x*17x")`)).to.equal("204 * x ^ 2")
+		})
+		it('output', function() {
+			expect(start(`output("Hello, World!")`)).to.equal(" Hello, World!")
 		})
 	})
 })
