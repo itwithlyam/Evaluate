5744c2a4b09bdc57bef243d930323e4fb78b6f67
Lyam Mosnier <lyam.mosnier@itwithlyam.co.uk>
2022-04-05 18:45:58 +0100

    ASM All the Way!

--- src/generator.js
+++ src/generator.js
@@ -2,14 +2,30 @@ import {writeFileSync} from 'fs';
 
 export default function Generator(AST, code, output) {
 
+    let stext = []
+    let sdata = []
+
+    code.forEach(section => {
+        console.log(section.type)
+        if (section.type === "text") stext.push(section.commands)
+        if (section.type === "data") sdata.push(section.commands)
+    })
+
     let nasm = `
         section .text
             global _start
 
             _start:
-                ${SMain}
+                ${stext.join("\n")}
+
+                jmp end
+
+            end:
+                mov eax,1
+                int 0x80
+
         section .data
-            ${SData}
+            ${sdata.join("\n")}
     `
 
     writeFileSync(output+".s", " ");
--- src/interpreter/standard/output.js
+++ src/interpreter/standard/output.js
@@ -4,8 +4,8 @@ import {simplify} from 'mathjs'
 export default function outputfunc(args, line, trace, compiled) {
 	try {
 		if (!compiled) return args.join(' ')
-		return ``
+		return [{commands: `mov eax,4\nmov ebx,1\nmov ecx,message\nmov edx,messagelen\nint 0x80\n`, type: "text"},{commands: `message db "${args.join(' ')}",10,0\nmessagelen equ $ - message`, type: "data"}]
 	} catch(err) {
 		throw new RuntimeError("StandardLibraryOutput", "An error occured during output", line, ParseTrace(trace))
-	}
+	} 
 }
