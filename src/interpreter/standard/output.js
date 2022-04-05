39e4f8879a1ec462127aa126925c0de31f030dd6
Lyam Mosnier <lyam.mosnier@itwithlyam.co.uk>
2022-04-05 07:20:32 +0100

    EVAL-4 I think we're running assembly, I don't even know anymore

--- src/interpreter/standard/output.js
+++ src/interpreter/standard/output.js
@@ -4,7 +4,7 @@ import {simplify} from 'mathjs'
 export default function outputfunc(args, line, trace, compiled) {
 	try {
 		if (!compiled) return args.join(' ')
-		return `printf("${args.join(' ')}\\n");`
+		return ``
 	} catch(err) {
 		throw new RuntimeError("StandardLibraryOutput", "An error occured during output", line, ParseTrace(trace))
 	}
