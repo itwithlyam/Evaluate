9ac25af206a86a22ef8880b27bba478991a29ed1
Lyam Mosnier <lyam.mosnier@itwithlyam.co.uk>
2022-04-06 17:08:45 +0100

    printf() requires id as first argument

--- src/interpreter/standard/output.js
+++ src/interpreter/standard/output.js
@@ -4,7 +4,7 @@ import {simplify} from 'mathjs'
 export default function outputfunc(args, line, trace, compiled) {
 	try {
 		if (!compiled) return args.join(' ')
-		return [{commands: `mov eax,4\nmov ebx,1\nmov ecx,message\nmov edx,messagelen\nint 0x80\n`, type: "text"},{commands: `message db "${args.join(' ')}",10,0\nmessagelen equ $ - message`, type: "data"}]
+		return [{commands: `mov eax,4\nmov ebx,1\nmov ecx,${args[0]}\nmov edx,${args[0]}len\nint 0x80\n`, type: "text"},{commands: `${args[0]} db "${args[1]}",10,0\n${args[0]}len equ $ - ${args[0]}`, type: "data"}]
 	} catch(err) {
 		throw new RuntimeError("StandardLibraryOutput", "An error occured during output", line, ParseTrace(trace))
 	} 
