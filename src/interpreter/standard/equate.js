1f9c4792917ea6a4e9f18918752504593165e0ff
Lyam Mosnier <lyam.mosnier@itwithlyam.co.uk>
2022-04-07 11:49:12 +0100

    EVAL-9 Standard Library compiled

--- src/interpreter/standard/equate.js
+++ src/interpreter/standard/equate.js
@@ -3,10 +3,11 @@ import {RuntimeError, ParseTrace} from '../../util.js'
 
 export default function eqfunc(args, line, trace, compiled) {
 	try {
-		let statement = args.join(' ').split(' ')
+		let statement = args[1].split(' ')
 		let a = equate.execute(statement, line)
 		if (!compiled) return a
-		return `printf("%d\\n", ${statement.join(' ')});`
+		return [{commands: `mov eax,4\nmov ebx,1\nmov ecx,${args[0]}\nmov edx,${args[0]}len\nint 0x80`, type: "text"}, {label: args[0], commands: `db "${args[0]}: ${a}",10,0`, type: "label"}, {label: args[0]+"len", commands: `equ $-${args[0]}`, type: "label"}]
+
 	} catch(err) {
 		console.log(err)
 		throw new RuntimeError("StandardLibraryEquate", "An error occured during evaluation", line, ParseTrace(trace))
