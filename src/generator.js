165bbe943e18f13df39221dbda20844168784e6f
Lyam Mosnier <lyam.mosnier@itwithlyam.co.uk>
2022-04-07 08:08:13 +0100

    Labels are easier to manage

--- src/generator.js
+++ src/generator.js
@@ -2,18 +2,30 @@ import {writeFileSync} from 'fs';
 
 export default function Generator(AST, code, output) {
 
+    // stext = section .text
+    // sdata = section .data
+    // labels = data within stext (i.e. varname: db "varcontent",10,0)
+
+    // Preprocessor
+
     let stext = []
     let sdata = []
+    let labels = []
 
     code.forEach(section => {
         console.log(section.type)
         if (section.type === "text") stext.push(section.commands)
         if (section.type === "data") sdata.push(section.commands)
+        if (section.type === "label") labels.push(`${section.label}: ${section.commands}`)
     })
+
+    // Generator
 
     let nasm = `
         section .text
             global _start
+
+            ${labels.join("\n")}
 
             _start:
                 ${stext.join("\n")}
@@ -24,8 +36,7 @@ export default function Generator(AST, code, output) {
                 mov eax,1
                 int 0x80
 
-        section .data
-            ${sdata.join("\n")}
+        ${sdata.join('\n')}
     `
 
     writeFileSync(output+".s", " ");
