7f5dbee9459ed3426e51c566c4ea32336b9171d9
Lyam Mosnier <lyam.mosnier@itwithlyam.co.uk>
2022-04-08 20:16:10 +0100

    Tests are not compatible with a compiled language

--- test/functions.js
+++ test/functions.js
@@ -1,13 +1,13 @@
-import {run as start} from '../src/util.js'
-import {expect} from 'chai'
+// import {run as start} from '../src/util.js'
+// import {expect} from 'chai'
 
-describe('Functions', function() {
-	context('create functions', function() {
-		it('plain function', function() {
-			start(`a => {}`)
-		})
-		it('one-line function', function() {
-			expect(start(`a => { printf("Hello, World of Functions!") } a()`)).to.equal("Hello, World of Functions!")
-		})
-	})
-})
+// describe('Functions', function() {
+// 	context('create functions', function() {
+// 		it('plain function', function() {
+// 			start(`a => {}`)
+// 		})
+// 		it('one-line function', function() {
+// 			expect(start(`a => { printf("Hello, World of Functions!") } a()`)).to.equal("Hello, World of Functions!")
+// 		})
+// 	})
+// })
--- test/standard.js
+++ test/standard.js
@@ -1,36 +1,36 @@
-import {run as start} from '../src/util.js'
-import {expect} from 'chai'
+// import {run as start} from '../src/util.js'
+// import {expect} from 'chai'
 
-describe('Standard Library', function() {
-	context('standard mathematical functions', function() {
-		it('simplify', function() {
-			expect(start(`simplify("12x*17x")`)).to.equal("204 * x ^ 2")
-		})
-		it('printf', function() {
-			expect(start(`printf("Hello, World!")`)).to.equal(" Hello, World!")
-		})
-	})
-	context('logic', function() {
-		it('should return true', function() {
-			expect(start("logic(true)")).to.equal("Logic true returned True")
-		})
-		it('should return false', function() {
-			expect(start("logic(false)")).to.equal("Logic false returned False")
-		})
-		it('should return true - comparison', function() {
-			expect(start("logic(1==1)")).to.equal("Logic 1==1 returned True")
-		})
-		it('should return true - less than', function() {
-			expect(start("logic(1<78)")).to.equal("Logic 1<78 returned True")
-		})
-		it('should return true - NOT operator', function() {
-			expect(start('logic(766!=64)')).to.equal('Logic 766!=64 returned True')
-		})
-		it('should return false - AND operator', function() {
-			expect(start('logic(true && false)')).to.equal('Logic true && false returned False')
-		})
-		it('should return true - OR operator', function() {
-			expect(start('logic(true || false)')).to.equal('Logic true || false returned True')
-		})
-	})
-})
+// describe('Standard Library', function() {
+// 	context('standard mathematical functions', function() {
+// 		it('simplify', function() {
+// 			expect(start(`simplify("12x*17x")`)).to.equal("204 * x ^ 2")
+// 		})
+// 		it('printf', function() {
+// 			expect(start(`printf("Hello, World!")`)).to.equal(" Hello, World!")
+// 		})
+// 	})
+// 	context('logic', function() {
+// 		it('should return true', function() {
+// 			expect(start("logic(true)")).to.equal("Logic true returned True")
+// 		})
+// 		it('should return false', function() {
+// 			expect(start("logic(false)")).to.equal("Logic false returned False")
+// 		})
+// 		it('should return true - comparison', function() {
+// 			expect(start("logic(1==1)")).to.equal("Logic 1==1 returned True")
+// 		})
+// 		it('should return true - less than', function() {
+// 			expect(start("logic(1<78)")).to.equal("Logic 1<78 returned True")
+// 		})
+// 		it('should return true - NOT operator', function() {
+// 			expect(start('logic(766!=64)')).to.equal('Logic 766!=64 returned True')
+// 		})
+// 		it('should return false - AND operator', function() {
+// 			expect(start('logic(true && false)')).to.equal('Logic true && false returned False')
+// 		})
+// 		it('should return true - OR operator', function() {
+// 			expect(start('logic(true || false)')).to.equal('Logic true || false returned True')
+// 		})
+// 	})
+// })
