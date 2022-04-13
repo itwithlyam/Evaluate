5290aca3a14cdf0cf05dc459f755f3f38ddb7436
Lyam Mosnier <lyam.mosnier@itwithlyam.co.uk>
2022-04-13 09:47:07 +0100

    Remove newline from end of output strings

--- src/interpreter/standard/output.js
+++ src/interpreter/standard/output.js
@@ -4,7 +4,7 @@ import {simplify} from 'mathjs'
 export default function outputfunc(args, line, trace, compiled) {
 	try {
 		if (!compiled) return args.join(' ')
-		return [{commands: `mov eax,4\nmov ebx,1\nmov ecx,${args[0]}\nmov edx,${args[0]}len\nint 0x80\n`, type: "text", os: ['win', 'linux']}, {commands: `mov eax,0x20000004\nmov ebx,0x20000001\nmov ecx,${args[0]}\nmov edx,${args[0]}len\nint 0x80\n`, type: "text", os: ['mac']}, {label: args[0], commands: `db "${args[0]}: ${args[1]}",10,0`, type: "label", os: ['mac', 'linux', 'win']}, {label: args[0]+"len", commands: `equ $-${args[0]}`, type: "label", os: ['mac', 'linux', 'win']}]
+		return [{commands: `mov eax,4\nmov ebx,1\nmov ecx,${args[0]}\nmov edx,${args[0]}len\nint 0x80\n`, type: "text", os: ['win', 'linux']}, {commands: `mov eax,0x20000004\nmov ebx,0x20000001\nmov ecx,${args[0]}\nmov edx,${args[0]}len\nint 0x80\n`, type: "text", os: ['mac']}, {label: args[0], commands: `db "${args[0]}: ${args[1]}",0`, type: "label", os: ['mac', 'linux', 'win']}, {label: args[0]+"len", commands: `equ $-${args[0]}`, type: "label", os: ['mac', 'linux', 'win']}]
 		//return [{commands: `mov eax,4\nmov ebx,1\nmov ecx,"${args[0]}"\nmov edx,${args[0].length}\nint 0x80\n`, type: "text"}]	
 	} catch(err) {
 		throw new RuntimeError("StandardLibraryOutput", "An error occured during output", line, ParseTrace(trace))
