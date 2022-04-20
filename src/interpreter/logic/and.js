a3c1bd6a5c17afaf0d042cb315ef4ffccc2439b6
lyammosnier1 <64089164+itwithlyam@users.noreply.github.com>
2022-04-20 13:11:18 +0000

    some stuff

--- src/interpreter/logic/and.js
+++ src/interpreter/logic/and.js
@@ -3,14 +3,21 @@ export default {
     description: "AND Boolean gate",
     execute(args) {
         return [
+					{
+						type: "bss",
+						id: "a",
+						mode: "resb",
+						bytes: 1,
+						os: ["mac", "win", "linux"]
+					},
             {
                 type: "text",
-                commands: `mov al,${args[0]}\nmov bl,${args[1]}\nand al,bl`,
+                commands: `mov eax,${args[0]}\nmov [a],eax\nand a,${args[1]}`,
                 os: ["mac", "win", "linux"],
             },
             {
                 type: "text",
-                commands: `mov eax,4\nmov ebx,1\nmov ecx,al\nmov edx,1\nint 0x80\n`,
+                commands: `mov eax,4\nmov ebx,1\nmov ecx,a\nmov edx,1\nint 0x80\n`,
                 os: ["win", "linux"],
             },
             {
