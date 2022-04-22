db4749025d8fb3aa29943a761858702265366015
lyammosnier1 <64089164+itwithlyam@users.noreply.github.com>
2022-04-22 12:51:38 +0000

    Booleans? What booleans?

--- src/interpreter/logic/not.js
+++ src/interpreter/logic/not.js
@@ -1,1 +1,7 @@
-export default {}
+export default {
+    name: "NOT",
+    description: "NOT Boolean gate",
+    execute(args) {
+				return !args[1]
+    }
+}
--- src/interpreter/logic/or.js
+++ src/interpreter/logic/or.js
@@ -1,1 +1,7 @@
-export default {}
+export default {
+    name: "OR",
+    description: "OR Boolean gate",
+    execute(args) {
+				return args[0] | args[1]
+    }
+}
