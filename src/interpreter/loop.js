5705e6156941418744bd21775c96baa0c5b2c657
lyammosnier1 <64089164+itwithlyam@users.noreply.github.com>
2022-04-27 13:11:42 +0000

    More loop stuff

--- src/interpreter/loop.js
+++ src/interpreter/loop.js
@@ -3,7 +3,21 @@ export default {
 	description: "iterates over a block of code",
 	execute(amount, block) {
 		let ans = []
-		for (let i = 0; i < amount; i++) block.forEach(e => ans.push(e))
-		return ans
+		for (let i = 0; i < amount; i++) block.forEach(e => {
+			if (e.type != 'text') return 
+			ans.push(e.commands)
+		})
+		return [
+			{ 
+				os: ['linux', 'win', 'mac'],
+				type: "text",
+				commands: ".a:\n" + ans.join('\n')
+			},
+			{
+				os: ['linux', 'win', 'mac'],
+				type: "text",
+				commands: `mov ecx,${amount}\nloop .a`
+			}
+		]
 	}
 }
