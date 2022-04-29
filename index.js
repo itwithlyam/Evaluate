455ed0210da5e52f623841e05ab2f5951b22a695
lyammosnier1 <64089164+itwithlyam@users.noreply.github.com>
2022-04-29 10:50:14 +0000

    Add some UX changes to index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const argdef = [
 	{name: 'version', type: Boolean},
 	{name: 'force', alias: 'f', type: Boolean},
 	{name: 'disable-warnings', type: Boolean},
-	{name: 'output', type: String, alias: 'o'}
+	{name: 'output', type: String, alias: 'o'},
+	{name: 'help', type: Boolean, alias: 'h'}
 ]
 
 import stopwatch from 'statman-stopwatch'
@@ -18,18 +19,35 @@ import chalk from 'chalk'
 import {Compile} from './src/compiler.js'
 import {Lexer} from './src/lexer.js'
 import {Parse} from './src/parser.js'
+import consola from 'consola'
+import { table } from 'table';
 import * as fs from 'fs'
 
 export default function runner() {
+	if (args(argdef).help) {
+		const flags = [
+			['Flag', 'Alias', 'Description'],
+			['--help', '-h', 'Shows this menu.']
+		]
+		const config = {
+			header: {
+				alignment: 'center',
+				content: 'CLI Help Panel: ' + version
+			}
+		}
+		console.log(table(flags, config))
+		if (devmode) consola.warn("Running devmode, use force")
+		return
+	}
 	try {
 		let verbose = false
 		const timer =  new stopwatch(true)
 		const options =  args(argdef)
 		if (!options['disable-warnings']) {
-			if (devmode) console.log(chalk.yellow("Warning: Current version is an unstable devmode. Expect bugs."))
+			if (devmode) consola.warn(chalk.yellow("Warning: Current version is an unstable devmode. Expect bugs."))
 		}
 		if (devmode && !options.force) {
-			console.log(chalk.red("To use devmode, please continue with force."))
+			consola.fatal(chalk.red("To use devmode, please continue with force."))
 			process.exit(1)
 		}
 		
