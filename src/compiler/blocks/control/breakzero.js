613e421efcb478308e9b6dc7528c5d4b9fab5da2
lyammosnier1 <64089164+itwithlyam@users.noreply.github.com>
2022-05-05 07:16:08 +0000

    testing converting from ASCII

--- src/compiler/blocks/control/breakzero.js
+++ src/compiler/blocks/control/breakzero.js
@@ -2,6 +2,6 @@ export default {
 	name: "breakzero",
 	description: "breaks out of a block if op1 is zero",
 	execute(args) {
-		return [{type: "text", os: ['win','mac','linux'], commands: `mov eax,[${args[0]}]\nsub eax,48\ntest eax,"0"\nje StrEnd\n`}]
+		return [{type: "text", os: ['win','mac','linux'], commands: `mov eax,[${args[0]}]\nadd eax,48\ntest eax,"0"\nje StrEnd\n`}]
 	}
 }
