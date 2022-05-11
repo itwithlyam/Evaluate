8d531d1cbcc00b484d8bda96f3c1a39b170d36b7
lyammosnier1 <64089164+itwithlyam@users.noreply.github.com>
2022-05-11 10:43:16 +0000

    It works!

--- converting.asm
+++ converting.asm
@@ -3,8 +3,7 @@
 
         section .text
 
-            global _start
-                        ConversionCheck:
+            ConversionCheck:
                             cmp al,0
                             jng StrEnd
 
@@ -58,97 +57,8 @@
 
                             jmp end
 
-            strrlabel:
-                db "Hello",0
-
-strrlen: equ 5
-strrloop: 
-                mov al, [ebx]
-
-                or al,al
-
-                jz StrEnd
-
-
-                cmp al,0
-
-                je StrEnd
-
-
-                add [strr+ecx], al
-
-
-                inc ebx
-
-                inc ecx
-
-                jmp strrloop
-
-              
-strprintf4: db 10,0
-strprintflen4: equ $-strprintf4
-ToConvert: db "1234567890",0
-len: equ 1000
-
-            StrEnd: ret
-
-            _start:
-								mov rbx, ToConvert
-								mov ecx,0
-								call ConvertToNum
-                                mov byte [Result+ecx],0
-
-                mov ebx, strrlabel
-
-                mov ecx, 0
-
-                call strrloop
-
-              
-
-mov eax,4
-mov ebx,1
-mov ecx,strr
-mov edx,strrlen
-int 0x80
-
-
-mov eax,4
-mov ebx,1
-mov ecx,strprintf4
-mov edx,strprintflen4
-int 0x80
-
-mov rbx, Result
-mov ecx,0
-call ConvertFromNum
-
-
-mov eax,4
-mov ebx,1
-mov ecx,ResultB
-mov ebx,len
-int 0x80
-
-mov eax,4
-mov ebx,1
-mov ecx,strprintf4
-mov edx,strprintflen4
-int 0x80
-
-
-
-                jmp end
-
-            end:
-                mov eax,1
-mov ebx,0
-                int 0x80
-
         
 
         section .bss
-strr resb 5
-printing resb 10
 Result resb 1000
 ResultB resb 1000
