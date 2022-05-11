745745484d8183ac1b449be4ac0164406fd0e628
Lyam Mosnier <lyam.mosnier@itwithlyam.co.uk>
2022-05-11 19:55:07 +0100

    Finishing touches

--- src/compiler/blocks/control/breaknotzero.js
+++ src/compiler/blocks/control/breaknotzero.js
@@ -2,6 +2,6 @@ export default {
 	name: "breaknotzero",
 	description: "breaks out of a block if op1 is not zero",
 	execute(args) {
-		return [{commands:`mov eax,[${args[0]}]\ncmp eax,"0"\njnz StrEnd\n`, os: ['win','linux','mac'], type: "text"}]
+		return [{commands:`mov eax,[${args[0]}]\ncmp eax,0\njnz StrEnd\n`, os: ['win','linux','mac'], type: "text"}]
 	}
 }
--- src/compiler/blocks/control/breakzero.js
+++ src/compiler/blocks/control/breakzero.js
@@ -2,6 +2,6 @@ export default {
 	name: "breakzero",
 	description: "breaks out of a block if op1 is zero",
 	execute(args) {
-		return [{commands:`mov eax,[${args[0]}]\ncmp eax,"0"\njz StrEnd\n`, os: ['win','linux','mac'], type: "text"}]
+		return [{commands:`mov eax,[${args[0]}]\ncmp eax,0\njz StrEnd\n`, os: ['win','linux','mac'], type: "text"}]
 	}
 }
