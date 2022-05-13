502691367adb2908b26f16e2e398c5c05f5f2be0
Lyam Mosnier <lyam.mosnier@itwithlyam.co.uk>
2022-05-13 08:34:58 +0100

    /r Carriage Return

--- src/compiler/standard/output.js
+++ src/compiler/standard/output.js
@@ -8,7 +8,7 @@ export default function outputfunc(args, line, trace, compiled, id) {
 		/* {label: args[0], commands: `db "${args[0]}: ${args[1]}",0`, type: "label", os: ['mac', 'linux', 'win']}, */
 		let counter = 0
 		let concat = false
-		args[0].split(/(\\n)|({)|(})|(\\a)|(\\b)|(\\t)/gi).forEach(l => {
+		args[0].split(/(\\n)|({)|(})|(\\a)|(\\b)|(\\t)|(\\r)/gi).forEach(l => {
 			if (!l) return
 			counter++
 			if (l === '\\n') {
@@ -35,7 +35,12 @@ export default function outputfunc(args, line, trace, compiled, id) {
 				ret.push({label: "printf"+id+"len"+counter, commands: `equ $-printf${id}${counter}`, type: "label", os: ['mac', 'linux', 'win']})
 				ret.push({commands: `mov eax,4\nmov ebx,1\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['win', 'linux']})
 				ret.push({commands: `mov eax,0x20000004\nmov ebx,0x20000001\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['mac']})
-			}else {
+			} else if (l === '\\r') {
+				ret.push({commands: `db 11,0`, type: "label", label: "printf"+id+counter, os: ['mac', 'linux', 'win']})
+				ret.push({label: "printf"+id+"len"+counter, commands: `equ $-printf${id}${counter}`, type: "label", os: ['mac', 'linux', 'win']})
+				ret.push({commands: `mov eax,4\nmov ebx,1\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['win', 'linux']})
+				ret.push({commands: `mov eax,0x20000004\nmov ebx,0x20000001\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['mac']})
+			} else {
 				if (concat) {
 					ret.push({commands: `mov eax,4\nmov ebx,1\nmov ecx,${l}\nmov edx,${l}len\nint 0x80\n`, type: "text", os: ['win', 'linux']})
 					ret.push({commands: `mov eax,0x20000004\nmov ebx,0x20000001\nmov ecx,${l}\nmov edx,${l}len\nint 0x80\n`, type: "text", os: ['mac']})
