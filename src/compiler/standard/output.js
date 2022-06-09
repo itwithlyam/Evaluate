e321be0dd74adff30808b7410b3879975c26019e
lyammosnier1 <64089164+itwithlyam@users.noreply.github.com>
2022-06-09 07:16:12 +0000

    Improve output() elf

--- src/compiler/standard/output.js
+++ src/compiler/standard/output.js
@@ -1,4 +1,4 @@
-import {RuntimeError, ParseTrace} from '../../util.js'
+import {RuntimeError, ParseTrace, ToHex} from '../../util.js'
 import {simplify} from 'mathjs'
 
 export default function outputfunc(args, line, trace, compiled, id) {
@@ -11,8 +11,10 @@ export default function outputfunc(args, line, trace, compiled, id) {
 			if (!l) return
 			counter++
 			if (l === '\\n') {
-				if (compiled) ret.push({
-					hex: ""
+				if (compiled) return ret.push({
+					hex: "0A", label: true, desc: "newline"
+				}, {
+					hex: "B804000000BB01000000B9__BA01000000CD80"
 				})
 				ret.push({commands: `db 10,0`, type: "label", label: "printf"+id+counter, os: ['mac', 'linux', 'win']})
 				ret.push({label: "printf"+id+"len"+counter, commands: `equ $-printf${id}${counter}`, type: "label", os: ['mac', 'linux', 'win']})
@@ -23,31 +25,66 @@ export default function outputfunc(args, line, trace, compiled, id) {
 			} else if (l === '}') {
 				concat = false
 			} else if (l === '\\a') {
+				if (compiled) return ret.push({
+					hex: "07", label: true, desc: "alert"
+				}, {
+					hex: "B804000000BB01000000B9__BA01000000CD80"
+				})
 				ret.push({commands: `db 7,0`, type: "label", label: "printf"+id+counter, os: ['mac', 'linux', 'win']})
 				ret.push({label: "printf"+id+"len"+counter, commands: `equ $-printf${id}${counter}`, type: "label", os: ['mac', 'linux', 'win']})
 				ret.push({commands: `mov eax,4\nmov ebx,1\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['win', 'linux']})
 				ret.push({commands: `mov eax,0x20000004\nmov ebx,0x20000001\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['mac']})
 			} else if (l === '\\b') {
+				if (compiled) return ret.push({
+					hex: "08", label: true, desc: "backspace"
+				}, {
+					hex: "B804000000BB01000000B9__BA01000000CD80"
+				})
 				ret.push({commands: `db 8,0`, type: "label", label: "printf"+id+counter, os: ['mac', 'linux', 'win']})
 				ret.push({label: "printf"+id+"len"+counter, commands: `equ $-printf${id}${counter}`, type: "label", os: ['mac', 'linux', 'win']})
 				ret.push({commands: `mov eax,4\nmov ebx,1\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['win', 'linux']})
 				ret.push({commands: `mov eax,0x20000004\nmov ebx,0x20000001\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['mac']})
 			} else if (l === '\\t') {
+				if (compiled) return ret.push({
+					hex: "09", label: true, desc: "newline"
+				}, {
+					hex: "B804000000BB01000000B9__BA01000000CD80"
+				})
 				ret.push({commands: `db 9,0`, type: "label", label: "printf"+id+counter, os: ['mac', 'linux', 'win']})
 				ret.push({label: "printf"+id+"len"+counter, commands: `equ $-printf${id}${counter}`, type: "label", os: ['mac', 'linux', 'win']})
 				ret.push({commands: `mov eax,4\nmov ebx,1\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['win', 'linux']})
 				ret.push({commands: `mov eax,0x20000004\nmov ebx,0x20000001\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['mac']})
 			} else if (l === '\\r') {
+				if (compiled) return ret.push({
+					hex: "0D", label: true, desc: "carriage return"
+				}, {
+					hex: "B804000000BB01000000B9__BA01000000CD80"
+				})
 				ret.push({commands: `db 13,0`, type: "label", label: "printf"+id+counter, os: ['mac', 'linux', 'win']})
 				ret.push({label: "printf"+id+"len"+counter, commands: `equ $-printf${id}${counter}`, type: "label", os: ['mac', 'linux', 'win']})
 				ret.push({commands: `mov eax,4\nmov ebx,1\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['win', 'linux']})
 				ret.push({commands: `mov eax,0x20000004\nmov ebx,0x20000001\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['mac']})
 			} else if (l === '\\f') {
+				if (compiled) return ret.push({
+					hex: "0C", label: true, desc: "form feed"
+				}, {
+					hex: "B804000000BB01000000B9__BA01000000CD80"
+				})
 				ret.push({commands: `db 12,0`, type: "label", label: "printf"+id+counter, os: ['mac', 'linux', 'win']})
 				ret.push({label: "printf"+id+"len"+counter, commands: `equ $-printf${id}${counter}`, type: "label", os: ['mac', 'linux', 'win']})
 				ret.push({commands: `mov eax,4\nmov ebx,1\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['win', 'linux']})
 				ret.push({commands: `mov eax,0x20000004\nmov ebx,0x20000001\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['mac']})
 			} else if (l === '\\v') {
+				if (compiled) {
+					ret.push({
+						hex: "0B", label: true, desc: "vertical tab"
+					})
+					ret.push({
+						hex: "B804000000BB01000000B9__BA01000000CD80"
+					})
+					return
+				}
+			
 				ret.push({commands: `db 11,0`, type: "label", label: "printf"+id+counter, os: ['mac', 'linux', 'win']})
 				ret.push({label: "printf"+id+"len"+counter, commands: `equ $-printf${id}${counter}`, type: "label", os: ['mac', 'linux', 'win']})
 				ret.push({commands: `mov eax,4\nmov ebx,1\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['win', 'linux']})
@@ -57,6 +94,13 @@ export default function outputfunc(args, line, trace, compiled, id) {
 					ret.push({commands: `mov eax,4\nmov ebx,1\nmov ecx,${l}\nmov edx,${l}len\nint 0x80\n`, type: "text", os: ['win', 'linux']})
 					ret.push({commands: `mov eax,0x20000004\nmov ebx,0x20000001\nmov ecx,${l}\nmov edx,${l}len\nint 0x80\n`, type: "text", os: ['mac']})
 				} else {
+					if (compiled) {
+						return ret.push({
+							hex: ToHex(l), label: true, desc: "text"
+							}, {
+								hex: `B804000000BB01000000B9__BA${parseMemoryAddress(ToHex(l).length / 2, 0)}CD80`
+						})
+					}
 					ret.push({commands: `db "${l}",0`, type: "label", label: "printf"+id+counter, os: ['mac', 'linux', 'win']})
 					ret.push({label: "printf"+id+"len"+counter, commands: `equ $-printf${id}${counter}`, type: "label", os: ['mac', 'linux', 'win']})
 					ret.push({commands: `mov eax,4\nmov ebx,1\nmov ecx,printf${id}${counter}\nmov edx,printf${id}len${counter}\nint 0x80\n`, type: "text", os: ['win', 'linux']})
